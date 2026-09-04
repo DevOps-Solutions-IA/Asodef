2faf13e124b16f7b14852e5d14423b7abf7c0231
DevOps  SOLUTIONS IA <devopssolutionsia@gmail.com>
2026-09-03 22:53:41 -0500

    refactor(payments): consume Master token service from orders

--- apps/api/src/modules/payments-lookup/payments-lookup.module.ts
+++ apps/api/src/modules/payments-lookup/payments-lookup.module.ts
@@ -1,14 +1,12 @@
 import { Module } from "@nestjs/common";
 import { MasterModule } from "../master/master.module";
 import { PaymentOrdersModule } from "../payment-orders/payment-orders.module";
-import { MasterPaymentSelectionTokenService } from "./master-payment-selection-token.service";
 import { PaymentsLookupController } from "./payments-lookup.controller";
 import { PaymentsLookupService } from "./payments-lookup.service";
 
 @Module({
   imports: [MasterModule, PaymentOrdersModule],
   controllers: [PaymentsLookupController],
-  providers: [PaymentsLookupService, MasterPaymentSelectionTokenService],
-  exports: [MasterPaymentSelectionTokenService],
+  providers: [PaymentsLookupService],
 })
 export class PaymentsLookupModule {}
