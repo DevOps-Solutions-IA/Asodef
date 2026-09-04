568fb0700577efde322c69157ed656e735f16308
DevOps  SOLUTIONS IA <devopssolutionsia@gmail.com>
2026-09-03 22:52:28 -0500

    feat(payments): wire Master selection tokens

--- apps/api/src/modules/payments-lookup/payments-lookup.module.ts
+++ apps/api/src/modules/payments-lookup/payments-lookup.module.ts
@@ -1,12 +1,14 @@
 import { Module } from "@nestjs/common";
 import { MasterModule } from "../master/master.module";
 import { PaymentOrdersModule } from "../payment-orders/payment-orders.module";
+import { MasterPaymentSelectionTokenService } from "./master-payment-selection-token.service";
 import { PaymentsLookupController } from "./payments-lookup.controller";
 import { PaymentsLookupService } from "./payments-lookup.service";
 
 @Module({
   imports: [MasterModule, PaymentOrdersModule],
   controllers: [PaymentsLookupController],
-  providers: [PaymentsLookupService],
+  providers: [PaymentsLookupService, MasterPaymentSelectionTokenService],
+  exports: [MasterPaymentSelectionTokenService],
 })
 export class PaymentsLookupModule {}
